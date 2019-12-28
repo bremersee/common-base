d1cd22612ab107dec73fe9bf22279d152722cd53
Christian Bremer <bremerseegooglemail.com>
2019-12-28 10:53:36 +0100

    Extends authentication properties.

--- common-base-autoconfigure/src/main/java/org/bremersee/security/authentication/AuthenticationProperties.java
+++ common-base-autoconfigure/src/main/java/org/bremersee/security/authentication/AuthenticationProperties.java
@@ -16,11 +16,23 @@
 
 package org.bremersee.security.authentication;
 
+import java.io.Serializable;
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.bremersee.security.core.AuthorityConstants;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * Authentication properties.
@@ -35,9 +47,87 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 public class AuthenticationProperties {
 
   /**
+   * Specifies whether Keycloak JWT beans should be created or not. Default is {@code false}.
+   *
+   * @see AuthenticationSupportAutoConfiguration
+   * @see ReactiveAuthenticationSupportAutoConfiguration
+   */
+  private boolean enableKeycloakSupport = false;
+
+  /**
+   * Properties for actuator endpoints.
+   */
+  private ActuatorAccessProperties actuator = new ActuatorAccessProperties();
+
+  /**
    * The properties for the oauth2 password flow.
    */
   private PasswordFlow passwordFlow = new PasswordFlow();
+
+  /**
+   * A list of in-memory users, that can login with basic authentication for testing purposes.
+   */
+  private List<SimpleUser> basicAuthUsers = new ArrayList<>();
+
+  /**
+   * Build basic auth user details.
+   *
+   * @return the user details
+   */
+  public UserDetails[] buildBasicAuthUserDetails() {
+    final PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    return getBasicAuthUsers().stream().map(
+        simpleUser -> User.builder()
+            .username(simpleUser.getName())
+            .password(simpleUser.getPassword())
+            .authorities(simpleUser.buildAuthorities())
+            .passwordEncoder(encoder::encode)
+            .build())
+        .toArray(UserDetails[]::new);
+  }
+
+  /**
+   * The type Actuator access properties.
+   */
+  @Getter
+  @Setter
+  @ToString
+  @EqualsAndHashCode
+  @NoArgsConstructor
+  public static class ActuatorAccessProperties implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * The roles which can access a protected actuator endpoint. The role names normally start with
+     * {@code ROLE_}.
+     */
+    private List<String> roles = new ArrayList<>();
+
+    /**
+     * The IP addresses which can access protected actuator endpoints without authentication.
+     */
+    private List<String> ipAddresses = new ArrayList<>();
+
+    /**
+     * Build access expression (SpEL) for actuator endpoints.
+     *
+     * @return the access expression (SpEL) for actuator endpoints
+     */
+    public String buildAccessExpression() {
+      Set<String> roleSet = new HashSet<>(roles);
+      if (roleSet.isEmpty()) {
+        roleSet.add(AuthorityConstants.ACTUATOR_ROLE_NAME);
+        roleSet.add(AuthorityConstants.ADMIN_ROLE_NAME);
+      }
+      final StringBuilder sb = new StringBuilder();
+      roleSet.forEach(
+          role -> sb.append(" or ").append("hasAuthority('").append(role).append("')"));
+      ipAddresses.forEach(
+          ipAddress -> sb.append(" or ").append("hasIpAddress('").append(ipAddress).append("')"));
+      return sb.toString().substring(" or ".length());
+    }
+  }
 
   /**
    * OAuth2 password flow configuration properties.
@@ -46,7 +136,9 @@ public class AuthenticationProperties {
   @Setter
   @ToString(exclude = {"clientSecret", "systemPassword"})
   @EqualsAndHashCode(exclude = {"clientSecret", "systemPassword"})
-  public static class PasswordFlow {
+  public static class PasswordFlow implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     private String tokenEndpoint;
 
@@ -58,11 +150,23 @@ public class AuthenticationProperties {
 
     private String systemPassword;
 
+    /**
+     * Create password flow properties for the system user.
+     *
+     * @return the password flow properties
+     */
     @SuppressWarnings("unused")
     public PasswordFlowProperties toProperties() {
       return this.toProperties(systemUsername, systemPassword);
     }
 
+    /**
+     * Create password flow properties for the given user.
+     *
+     * @param username the username
+     * @param password the password
+     * @return the password flow properties
+     */
     public PasswordFlowProperties toProperties(String username, String password) {
       return PasswordFlowProperties.builder()
           .username(username)
@@ -72,7 +176,49 @@ public class AuthenticationProperties {
           .password(password)
           .build();
     }
+  }
 
+  /**
+   * A simple user.
+   */
+  @Getter
+  @Setter
+  @ToString(exclude = "password")
+  @EqualsAndHashCode(exclude = "password")
+  @NoArgsConstructor
+  public static class SimpleUser implements Serializable, Principal {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * The user name of the user.
+     */
+    private String name;
+
+    /**
+     * The password of the user.
+     */
+    private String password;
+
+    /**
+     * The granted authorities of the user. The names of the authorities starts with {@code ROLE_}.
+     */
+    private List<String> authorities = new ArrayList<>();
+
+    /**
+     * Build authorities.
+     *
+     * @return the authorities
+     */
+    String[] buildAuthorities() {
+      if (authorities == null || authorities.isEmpty()) {
+        return new String[]{
+            AuthorityConstants.USER_ROLE_NAME,
+            AuthorityConstants.LOCAL_USER_ROLE_NAME
+        };
+      }
+      return authorities.toArray(new String[0]);
+    }
   }
 
 }
--- common-base-autoconfigure/src/main/java/org/bremersee/security/authentication/AuthenticationSupportAutoConfiguration.java
+++ common-base-autoconfigure/src/main/java/org/bremersee/security/authentication/AuthenticationSupportAutoConfiguration.java
@@ -3,6 +3,8 @@ package org.bremersee.security.authentication;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication.Type;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
@@ -15,6 +17,10 @@ import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.util.Assert;
 
 @ConditionalOnWebApplication(type = Type.SERVLET)
+@ConditionalOnProperty(
+    prefix = "bremersee.security.authentication",
+    name = "enable-keycloak-support",
+    havingValue = "true")
 @ConditionalOnClass({
     org.springframework.boot.web.client.RestTemplateBuilder.class,
     org.bremersee.security.authentication.KeycloakJwtConverter.class,
@@ -41,11 +47,14 @@ public class AuthenticationSupportAutoConfiguration {
         getClass().getSimpleName());
   }
 
+  @ConditionalOnMissingBean
   @Bean
   public KeycloakJwtConverter keycloakJwtConverter() {
+    log.info("Creating {} ...", KeycloakJwtConverter.class.getSimpleName());
     return new KeycloakJwtConverter();
   }
 
+  @ConditionalOnMissingBean
   @Bean
   public RestTemplateAccessTokenRetriever restTemplateAccessTokenRetriever(
       ObjectProvider<RestTemplateBuilder> restTemplateBuilder) {
@@ -53,9 +62,11 @@ public class AuthenticationSupportAutoConfiguration {
     Assert.notNull(
         restTemplateBuilder.getIfAvailable(),
         "Rest template builder must be present.");
+    log.info("Creating {} ...", RestTemplateAccessTokenRetriever.class.getSimpleName());
     return new RestTemplateAccessTokenRetriever(restTemplateBuilder.getIfAvailable().build());
   }
 
+  @ConditionalOnMissingBean
   @Bean
   public PasswordFlowAuthenticationManager passwordFlowAuthenticationManager(
       ObjectProvider<JwtDecoder> jwtDecoder,
@@ -66,6 +77,7 @@ public class AuthenticationSupportAutoConfiguration {
         jwtDecoder.getIfAvailable(),
         "Jwt decoder must be present.");
 
+    log.info("Creating {} ...", PasswordFlowAuthenticationManager.class.getSimpleName());
     return new PasswordFlowAuthenticationManager(
         properties,
         jwtDecoder.getIfAvailable(),
