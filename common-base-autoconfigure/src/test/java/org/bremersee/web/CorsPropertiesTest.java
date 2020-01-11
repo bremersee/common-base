ba1e725db63587201a209049e65e0513598af068
Christian Bremer <bremerseegooglemail.com>
2020-01-11 18:15:57 +0100

    Adds unit tests for autoconfigure3.

--- common-base-autoconfigure/src/test/java/org/bremersee/web/CorsPropertiesTest.java
+++ common-base-autoconfigure/src/test/java/org/bremersee/web/CorsPropertiesTest.java
@@ -1,27 +1,43 @@
+/*
+ * Copyright 2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.bremersee.web;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Arrays;
 import java.util.List;
 import org.bremersee.web.CorsProperties.CorsConfiguration;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 /**
  * The cors properties test.
  *
  * @author Christian Bremer
  */
-public class CorsPropertiesTest {
+class CorsPropertiesTest {
 
   /**
    * Tests allow all configuration.
    */
   @Test
-  public void allowAllConfiguration() {
+  void allowAllConfiguration() {
     List<CorsConfiguration> configurations = CorsProperties.allowAllConfiguration();
     assertNotNull(configurations);
     assertTrue(configurations.contains(CorsConfiguration.allowAllConfiguration()));
@@ -31,7 +47,7 @@ public class CorsPropertiesTest {
    * Tests configs.
    */
   @Test
-  public void configs() {
+  void configs() {
     CorsProperties properties = new CorsProperties();
     properties.setAllowAll(true);
     List<CorsConfiguration> configurations = properties.getConfigs();
@@ -92,7 +108,7 @@ public class CorsPropertiesTest {
    * Tests allow all.
    */
   @Test
-  public void allowAll() {
+  void allowAll() {
     CorsProperties properties = new CorsProperties();
     properties.setAllowAll(true);
     assertTrue(properties.isAllowAll());
@@ -104,7 +120,7 @@ public class CorsPropertiesTest {
    * Tests to string and equals.
    */
   @Test
-  public void toStringAndEquals() {
+  void toStringAndEquals() {
     CorsProperties cp0 = new CorsProperties();
     cp0.setConfigs(CorsProperties.allowAllConfiguration());
     assertEquals(cp0, cp0);
