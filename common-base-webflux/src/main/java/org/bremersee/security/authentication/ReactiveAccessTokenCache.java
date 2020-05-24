f29a3f200acc18e18a71e04bf0c8bd59c7b73870
Christian Bremer <bremersee@googlemail.com>
2020-05-24 13:41:14 +0200

    Version 2.1.3

--- common-base-webflux/src/main/java/org/bremersee/security/authentication/ReactiveAccessTokenCache.java
+++ common-base-webflux/src/main/java/org/bremersee/security/authentication/ReactiveAccessTokenCache.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.bremersee.security.authentication;
 
 import javax.validation.constraints.NotNull;
@@ -5,7 +21,9 @@ import org.springframework.validation.annotation.Validated;
 import reactor.core.publisher.Mono;
 
 /**
- * The interface Reactive access token cache.
+ * The reactive access token cache interface.
+ *
+ * @author Christian Bremer
  */
 @Validated
 public interface ReactiveAccessTokenCache {
@@ -13,6 +31,7 @@ public interface ReactiveAccessTokenCache {
   /**
    * The constant CACHE_NAME.
    */
+  @SuppressWarnings("unused")
   String CACHE_NAME = AccessTokenCache.CACHE_NAME;
 
   /**
@@ -32,6 +51,12 @@ public interface ReactiveAccessTokenCache {
    */
   Mono<String> putAccessToken(@NotNull String key, @NotNull String accessToken);
 
+  /**
+   * Creates a reactive cache from the given access token cache.
+   *
+   * @param accessTokenCache the access token cache
+   * @return the reactive access token cache
+   */
   static ReactiveAccessTokenCache from(@NotNull AccessTokenCache accessTokenCache) {
     return new ReactiveAccessTokenCache() {
       @Override
